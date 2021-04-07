492607b38fdfcfe5942e7ef1855c14dcba9f60df
2856672598 <2856672598@qq.com>
2021-04-07 17:11:01 +0800

    4/7上课代码

--- class4-1/class4-1/text.c
+++ class4-1/class4-1/text.c
@@ -43,51 +43,124 @@
 //	printf("%d", max);
 //}
 
+//#include<stdio.h>
+//int main()
+//{
+//	int n;
+//	scanf("%d", &n);
+//	switch (n)
+//	{
+//	case 1:
+//		printf("一月\n");
+//		break;
+//	case 2:
+//		printf("二月\n");
+//		break;
+//	case 3:
+//		printf("三月\n");
+//		break;
+//	case 4:
+//		printf("四月\n");
+//		break;
+//	case 5:
+//		printf("五月\n");
+//		break;
+//	case 6:
+//		printf("六月\n");
+//		break;
+//	case 7:
+//		printf("七月\n");
+//		break;
+//	case 8:
+//		printf("八月\n");
+//		break;
+//	case 9:
+//		printf("九月\n");
+//		break;
+//	case 10:
+//		printf("十月\n");
+//		break;
+//	case 11:
+//		printf("十一月\n");
+//		break;
+//	case 12:
+//		printf("十二月\n");
+//		break;
+//	default:
+//		printf("error\n");
+//	}
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int n;
+//	scanf("%d", &n);
+//	if (n >= 0 && n <= 100)
+//		n /= 10;
+//	else
+//	{
+//		printf("分数错误\n");
+//		return 0;
+//	}
+//	switch (n)
+//	{
+//	case 10:
+//	case 9:
+//		printf("优秀\n");
+//		break;
+//	case 8:
+//		printf("良\n");
+//		break;
+//	case 7:
+//		printf("及格\n");
+//		break;
+//	case 6 :
+//	case 5:
+//	case 4:
+//	case 3:
+//	case 2:
+//	case 1:
+//	case 0:
+//		printf("不及格\n");
+//		break;
+//	}
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	double weight, height, bmi;
+//	printf("请输入身高（m），体重（kg):");
+//	scanf("%lf %lf", &height, &weight);
+//	bmi = weight / (height*height);
+//	if (bmi < 18.5)
+//		printf("偏瘦,BMI指数为%lf\n", bmi);
+//	else if (bmi <= 24.9)
+//		printf("正常,BMI指数为%lf\n", bmi);
+//	else if (bmi <= 27.9)
+//		printf("超重,BMI指数为%lf\n", bmi);
+//	else
+//		printf("肥胖,BMI指数为%lf\n", bmi);
+//	return 0;
+//}
+
+
 #include<stdio.h>
 int main()
 {
-	int n;
-	scanf("%d", &n);
-	switch (n)
-	{
-	case 1:
-		printf("一月\n");
-		break;
-	case 2:
-		printf("二月\n");
-		break;
-	case 3:
-		printf("三月\n");
-		break;
-	case 4:
-		printf("四月\n");
-		break;
-	case 5:
-		printf("五月\n");
-		break;
-	case 6:
-		printf("六月\n");
-		break;
-	case 7:
-		printf("七月\n");
-		break;
-	case 8:
-		printf("八月\n");
-		break;
-	case 9:
-		printf("九月\n");
-		break;
-	case 10:
-		printf("十月\n");
-		break;
-	case 11:
-		printf("十一月\n");
-		break;
-	case 12:
-		printf("十二月\n");
-		break;
-	default:
-		printf("error\n");
-	}
+	int x, y;
+	scanf("%d", &x);
+	if (x < -10)
+		y = 10;
+	else if (x <= 10)
+		y = 0;
+	else
+		y = -10;
+	printf("%d", y);
 	return 0;
 }
