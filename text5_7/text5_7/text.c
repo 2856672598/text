1805596a7c042a3c35e7eb27673024a9de0c802d
2856672598 <2856672598@qq.com>
2021-05-08 23:14:50 +0800

    带头双向循环链表增删查改

--- text5_7/text5_7/text.c
+++ text5_7/text5_7/text.c
@@ -116,85 +116,311 @@
 //	return 0;
 //}
 
-//链式栈
+////链式栈
+//
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<assert.h>
+//#include<stdbool.h>
+//typedef int DataType;
+//typedef struct Node
+//{
+//	DataType data;
+//	struct Node* next;
+//}Node;
+//enum
+//{
+//	EXIT,
+//	PUSH,
+//	POP,
+//	PRINT,
+//	LENGTH
+//};
+//void menu()
+//{
+//	printf("##################################\n");
+//	printf("####1.PUSH              2.POP#####\n");
+//	printf("####3.PRINT            0.EXIT#####\n");
+//	printf("############4.LENGTH #############\n");
+//}
+//Node* BuyNode(DataType data)
+//{
+//	Node* NewNode = malloc(sizeof(Node));
+//	if (NewNode)
+//	{
+//		NewNode->data = data;
+//		NewNode->next = NULL;
+//	}
+//	return NewNode;
+//}
+///*入栈采用头插法*/
+//void Push(Node** list, DataType data)
+//{
+//	Node* NewNode = BuyNode(data);
+//	assert(NewNode);
+//	if (*list == NULL)
+//	{
+//		*list = NewNode;
+//		return;
+//	}
+//	NewNode->next = *list;
+//	*list = NewNode;
+//	printf("入栈成功\n");
+//}
+////出栈
+//void Pop(Node** list)
+//{
+//	if (*list)
+//	{
+//		Node* next = (*list)->next;
+//		printf("%d出栈成功\n", (*list)->data);
+//		free(*list);
+//		*list = next;
+//		return;
+//	}
+//	printf("出栈失败\n");
+//}
+//void Print(Node* list)
+//{
+//	if (list)
+//	{
+//		Node* cur = list;
+//		while (cur)
+//		{
+//			printf("%d ", cur->data);
+//			cur = cur->next;
+//		}
+//		printf("\n");
+//		return;
+//	}
+//	printf("栈空\n");
+//}
+//int Length(Node* list)
+//{
+//	int size = 0;
+//	while (list)
+//	{
+//		size++;
+//		list = list->next;
+//	}
+//	return size;
+//}
+//void text()
+//{
+//	Node* Stack = NULL;
+//	int data, input, size;
+//	do
+//	{
+//		menu();
+//		printf("请选择:");
+//		scanf("%d", &input);
+//		switch (input)
+//		{
+//		case EXIT:
+//			printf("退出\n");
+//			return;
+//		case PUSH:
+//			printf("输入数据:");
+//			scanf("%d", &data);
+//			Push(&Stack, data);
+//			break;
+//		case POP:
+//			Pop(&Stack);
+//			break;
+//		case PRINT:
+//			Print(Stack);
+//			break;
+//		case LENGTH:
+//			size = Length(Stack);
+//			printf("栈中共有%d个节点\n", size);
+//			break;
+//		default:
+//			printf("输入错误");
+//			break;
+//		}
+//	} while (input);
+//}
+//int main()
+//{
+//	text();
+//	return 0;
+//}
+
+//带头双向循环链表增删查改
 #include<stdio.h>
 #include<stdlib.h>
 #include<assert.h>
-#include<stdbool.h>
 typedef int DataType;
-typedef struct Node
+typedef struct ListNode
 {
+	struct ListNode* next;
+	struct ListNode* prev;
 	DataType data;
-	struct Node* next;
-}Node;
+}ListNode;
 enum
 {
 	EXIT,
-	PUSH,
-	POP,
+	PUSHBACK,
+	PUSHFRONT,
+	POPBACK,
+	POPFRONT,
+	FIND,
+	INSERT,
 	PRINT,
+	ERASE
 };
 void menu()
 {
-	printf("##################################\n");
-	printf("####1.PUSH              2.POP#####\n");
-	printf("####3.PRINT            4.EXIT#####\n");
-	printf("##################################\n");
+	printf("************************************\n");
+	printf("****1.PushBack       2.PushFront****\n");
+	printf("****3.PopBack         4.PopFront****\n");
+	printf("****5.Find              6.Insert****\n");
+	printf("****7.Print              8.Erase****\n");
+	printf("************ 0.EXIT ****************\n");
 }
-Node* BuyNode(DataType data)
+ListNode* BuyListNode(DataType x)
 {
-	Node* NewNode = malloc(sizeof(Node));
-	if (NewNode)
-	{
-		NewNode->data = data;
-		NewNode->next = NULL;
-	}
-	return NewNode;
+	ListNode* newnode = malloc(sizeof(ListNode));
+	newnode->next = NULL;
+	newnode->prev = NULL;
+	newnode->data = x;
+	return newnode;
 }
-void Push(Node** list, DataType data)
+ListNode* ListInit()
 {
-	Node* NewNode = BuyNode(data);
-	assert(NewNode);
-	if (*list == NULL)
+	ListNode* newnode = BuyListNode(0);
+	assert(newnode);
+	newnode->next = newnode;
+	newnode->prev = newnode;
+	return newnode;
+}
+void ListPushBack(ListNode* phead, DataType x)
+{
+	ListNode* tail = phead->prev;
+	ListNode* next = BuyListNode(x);
+	tail->next = next;
+	next->prev = tail;
+	next->next = phead;
+	phead->prev = next;
+	printf("添加成功\n");
+}
+void ListPushFront(ListNode* phead, DataType x)
+{
+	ListNode* first = BuyListNode(x);
+	ListNode* next = phead->next;
+	first->prev = phead;
+	first->next = next;
+	next->prev = first;
+	phead->next = first;
+	printf("头插成功\n");
+}
+void ListPrint(ListNode* phead)
+{
+	ListNode* cur = phead->next;
+	if (cur == phead)
 	{
-		*list = NewNode;
+		printf("链表为空\n");
 		return;
 	}
-	NewNode->next = *list;
-	*list = NewNode;
-	printf("入栈成功\n");
-}
-//出栈
-void Pop(Node** list)
-{
-	if (*list)
+	while (cur != phead)
 	{
-		Node* next = (*list)->next;
-		free(*list);
-		*list = next;
-		printf("出栈成功\n");
+		printf("%d ", cur->data);
+		cur = cur->next;
+	}
+	printf("\n");
+}
+void ListPopBack(ListNode* phead)
+{
+	assert(phead);
+	if (phead->next == phead)
+	{
+		printf("链表为空\n");
 		return;
 	}
-	printf("出栈失败\n");
+	ListNode* tail = phead->prev;
+	tail->prev->next = phead;
+	phead->prev = tail->prev;
+	free(tail);
+	printf("尾删成功\n");
 }
-void Print(Node* list)
+void ListPopFront(ListNode* phead)
 {
-	if (list)
+	assert(phead);
+	//防止头结点被误删
+	if (phead->next == phead)
 	{
-		Node* cur = list;
-		while (cur)
-		{
-			printf("%d ", cur->data);
-			cur = cur->next;
-		}
-		printf("\n");
+		printf("链表为空\n");
 		return;
 	}
-	printf("栈空\n");
+	ListNode* cur = phead->next;
+	phead->next = cur->next;
+	cur->next->prev = phead;
+	printf("头删成功\n");
 }
-void text()
+ListNode* ListFind(ListNode* phead, DataType x)
 {
-	Node* Stack = NULL;
+	ListNode* cur = phead->next;
+	while (cur != phead)
+	{
+		if (cur->data == x)
+			return cur;
+		cur = cur->next;
+	}
+	return NULL;
+}
+//指定插入
+void ListInsert(ListNode* pos, DataType x)
+{
+	//判断插入的位置是不是哨兵节点（头结点）
+	if (pos->next == pos)
+	{
+		printf("添加失败\n");
+		return;
+	}
+	ListNode* newnode = BuyListNode(x);
+	ListNode* next = pos->next;
+	newnode->next = next;
+	next->prev = newnode;
+	pos->next = newnode;
+	newnode->prev = pos;
+	printf("添加成功\n");
+}
+//指定删除
+void ListErase(ListNode* pos)
+{
+	if (pos == NULL)
+	{
+		printf("链表中不存在此数据\n");
+		return;
+	}
+	if (pos->next == pos);
+	{
+		//输入的位置是哨兵节点
+		printf("此位置不可以删除\n");
+		return;
+	}
+	ListNode* next = pos->next;
+	ListNode* prev = pos->prev;
+	prev->next = next;
+	next->prev = prev;
+	printf("删除成功\n");
+}
+void ListDestory(ListNode*phead)
+{
+	ListNode* cur = phead->next;
+	while (cur != phead)
+	{
+		ListNode* next = cur->next;
+		free(cur);
+		cur = next;
+	}
+	free(phead);
+}
+
+void Text()
+{
+	ListNode* plist = ListInit();
+	ListNode* flag = NULL;
 	int data, input;
 	do
 	{
@@ -203,20 +429,61 @@ void text()
 		scanf("%d", &input);
 		switch (input)
 		{
-		case PUSH:
-			printf("输入数据:");
-			scanf("%d", &data);
-			Push(&Stack, data);
-			break;
-		case POP:
-			Pop(&Stack);
-			break;
-		case PRINT:
-			Print(Stack);
-			break;
 		case EXIT:
+			ListDestory(plist);
 			printf("退出\n");
 			return;
+			break;
+		case PUSHBACK:
+			printf("请输入数据:");
+			scanf("%d", &data);
+			ListPushBack(plist, data);
+			break;
+		case PUSHFRONT:
+			printf("请输入数据:");
+			scanf("%d", &data);
+			ListPushFront(plist, data);
+			break;
+		case POPBACK:
+			ListPopBack(plist);
+			break;
+		case POPFRONT:
+			ListPopFront(plist);
+			break;
+		case FIND:
+			printf("输入要查找的数据:");
+			scanf("%d", &data);
+			flag = ListFind(plist, data);
+			printf("%d的地址为%p\n", data, flag);
+			break;
+		case INSERT:
+			printf("输入要选择插入的位置:");
+			scanf("%d", &data);
+			flag = ListFind(plist, data);
+			if (flag != NULL)
+			{
+				printf("输入要插入的数据:");
+				scanf("%d", &data);
+				ListInsert(flag, data);
+				printf("插入成功\n");
+			}
+			else
+				printf("%d不存在链表中\n", data);
+			break;
+		case PRINT:
+			ListPrint(plist);
+			break;
+		case ERASE:
+			printf("输入要删除的数据:");
+			scanf("%d", &data);
+			flag = ListFind(plist, data);
+			if (flag != plist)
+			{
+				//保证删除的节点不是头结点
+				//flag可能为NULL
+				ListErase(flag);
+			}
+			break;
 		default:
 			printf("输入错误");
 			break;
@@ -225,6 +492,6 @@ void text()
 }
 int main()
 {
-	text();
+	Text();
 	return 0;
 }
