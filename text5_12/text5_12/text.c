6101985b19093416fd64a0a36c3a6dcf0746b84a
2856672598 <2856672598@qq.com>
2021-05-14 23:14:34 +0800

    5/14链表oj

--- text5_12/text5_12/text.c
+++ text5_12/text5_12/text.c
@@ -56,47 +56,130 @@
 //	return slow;
 //}
 
-//21. 合并两个有序链表
+////21. 合并两个有序链表
+//#include<stdio.h>
+////https://leetcode-cn.com/problems/merge-two-sorted-lists/
+////struct ListNode {
+////	int val;
+////	struct ListNode *next;
+////};
+////struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2)
+////{
+////	if (l1 == NULL || l2 == NULL)
+////		return l1 == NULL ? l2 : l1;
+////	struct ListNode* head = NULL;
+////	if (l1->val < l2->val)
+////	{
+////		head = l1;
+////		l1 = l1->next;
+////	}
+////	else
+////	{
+////		head = l2;
+////		l2 = l2->next;
+////	}
+////	struct ListNode* cur = head;
+////	while (l1 != NULL && l2 != NULL)
+////	{
+////		if (l1->val < l2->val)
+////		{
+////			cur->next = l1;
+////			cur = l1;
+////			l1 = l1->next;
+////		}
+////		else
+////		{
+////			cur->next = l2;
+////			cur = l2;
+////			l2 = l2->next;
+////		}
+////	}
+////	if (l1 == NULL)
+////		cur->next = l2;
+////	if (l2 == NULL)
+////		cur->next = l1;
+////	return head;
+////}
+//
+////带哨兵位的头节点
+//struct ListNode {
+//	int val;
+//	struct ListNode *next;
+//};
+//struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2)
+//{
+//	struct ListNode newhead;
+//	struct ListNode* cur = &newhead;
+//	while (l1 != NULL && l2 != NULL)
+//	{
+//		if (l1->val > l2->val)
+//		{
+//			cur->next = l2;
+//			cur = l2;
+//			l2 = l2->next;
+//		}
+//		else
+//		{
+//			cur->next = l1;
+//			cur = l1;
+//			l1 = l1->next;
+//		}
+//	}
+//	if (l1 == NULL)
+//		cur->next = l2;
+//	if (l2 == NULL)
+//		cur->next = l1;
+//	return newhead.next;
+//}
+
+
+////环形链表
+//#include<stdio.h>
+//#include<stdbool.h>
+//struct ListNode {
+//	int val;
+//	struct ListNode *next;
+//};
+//
+//bool hasCycle(struct ListNode *head)
+//{
+//	struct ListNode* fast = head, *slow = head;
+//	while (fast != NULL && fast->next != NULL)
+//	{
+//		fast = fast->next->next;
+//		slow = slow->next;
+//		if (fast == slow)
+//			return true;
+//	}
+//	return false;
+//}
+
+
+//142. 环形链表 II
 #include<stdio.h>
-//https://leetcode-cn.com/problems/merge-two-sorted-lists/
-struct ListNode {
-	int val;
-	struct ListNode *next;
-};
-struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2)
+struct ListNode
 {
-	if (l1 == NULL || l2 == NULL)
-		return l1 == NULL ? l2 : l1;
-	struct ListNode* head = NULL;
-	if (l1->val < l2->val)
+	int val;
+	struct ListNode *next
+};
+struct ListNode *detectCycle(struct ListNode *head)
+{
+	struct ListNode* fast = head, *slow = head;
+	while (fast && fast->next)
 	{
-		head = l1;
-		l1 = l1->next;
+		fast = fast->next->next;
+		slow = slow->next;
+		if (fast == slow)
+			break;
 	}
-	else
-	{
-		head = l2;
-		l2 = l2->next;
-	}
+	//到这分为两种情况1是fast为空了，或找到了相遇点
+	if (fast == NULL || fast->next == NULL)
+		return NULL;
 	struct ListNode* cur = head;
-	while (l1 != NULL && l2 != NULL)
+	while (cur != slow)
 	{
-		if (l1->val < l2->val)
-		{
-			cur->next = l1;
-			cur = l1;
-			l1 = l1->next;
-		}
-		else
-		{
-			cur->next = l2;
-			cur = l2;
-			l2 = l2->next;
-		}
+		cur = cur->next;
+		slow = slow->next;
 	}
-	if (l1 == NULL)
-		cur->next = l2;
-	if (l2 == NULL)
-		cur->next = l1;
-	return head;
+	return cur;
 }
