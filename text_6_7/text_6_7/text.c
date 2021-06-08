347a8bcd093f8c78e31ad1faee3aea6adc53b8f3
2856672598 <2856672598@qq.com>
2021-06-08 21:57:32 +0800

    6/8练习

--- text_6_7/text_6_7/text.c
+++ text_6_7/text_6_7/text.c
@@ -39,26 +39,33 @@ char * addStrings(char * num1, char * num2)
 	int size2 = strlen(num2);
 
 	int size = size1 > size2 ? size1 : size2;
-	char* arr = malloc(sizeof(char)*(size + 1));
+	char* arr = malloc(sizeof(char)*(size + 2));
 	int i = size1 - 1, j = size2 - 1, len = 0;
 
 	int flag = 0;
 	while (i >= 0 || j >= 0)
 	{
-
-		int sum = num1[i] + num2[j] + flag - '0' - '0';
+		char data1 = '0', data2 = '0';
+		if (i >= 0)
+			data1 = num1[i];
+		if (j >= 0)
+			data2 = num2[j];
+		int sum = data1 + data2 + flag - '0' - '0';
 		if (sum > 9)
 		{
 			sum %= 10;
 			flag = 1;
 		}
+		else
+			flag = 0;
 		arr[len++] = sum + '0';
 		i--;
 		j--;
 	}
 	if (flag == 1)
-		arr[len] = '1';
-	int left = 0, right = len;
+		arr[len++] = '1';
+	int left = 0, right = len - 1;
+	//·­×ª
 	while (left < right)
 	{
 		char tmp = arr[left];
@@ -67,13 +74,58 @@ char * addStrings(char * num1, char * num2)
 		left++;
 		right--;
 	}
+	if (flag == 1)
+		arr[size + 1] = '\0';
+	else
+		arr[size] = '\0';
 	return arr;
 }
 
+
+char * addBinary(char * a, char * b)
+{
+	int sizea = strlen(a), sizeb = strlen(b);
+	int size = sizea > sizeb ? sizea + 2 : sizeb + 2;
+	char* arr = malloc(sizeof(char)*size);
+	int i = sizea - 1, j = sizeb - 1, flag = 0, len = 0;
+	while (i >= 0 || j >= 0)
+	{
+		int data1 = 0, data2 = 0;
+		if (i >= 0)
+			data1 = a[i] - '0';
+		if (j >= 0)
+			data2 = b[j] - '0';
+		int sum = data1 + data2 + flag;
+		if (sum >= 2)
+		{
+			sum %= 2;
+			flag = 1;
+		}
+		else
+			flag = 0;
+		arr[len++] = sum + '0';
+		i--;
+		j--;
+	}
+	if (flag == 1)
+		arr[len++] = '1';
+	int left = 0, right = len - 1;
+	//·­×ª
+	while (left < right)
+	{
+		char tmp = arr[left];
+		arr[left] = arr[right];
+		arr[right] = tmp;
+		left++;
+		right--;
+	}
+	arr[len] = '\0';
+	return arr;
+}
 int main()
 {
-	char num1[] = "123";
-	char num2[] = "1";
-	printf("%s", addStrings(num1, num2));
+	char arr1[] = "11";
+	char arr2[] = "1";
+	printf("%s", addBinary(arr1, arr2));
 	return 0;
 }
