36d8c4722ca5f798a9a9e84c497b176d89a23bef
2856672598 <2856672598@qq.com>
2021-06-19 21:53:39 +0800

    练习

--- text6_18/text6_18/text.c
+++ text6_18/text6_18/text.c
@@ -72,20 +72,21 @@
 //	int sz = sizeof(arr) / sizeof(arr[0]);
 //	int left = 0, right = sz - 1;
 //	int min = 0;
+//	int n = 19;
 //	while (left <= right)
 //	{
 //		min = (left + right) / 2;
-//		if (arr[min] > 6)
+//		if (arr[min] > n)
 //		{
 //			right = min - 1;
 //		}
-//		else if (arr[min] < 6)
+//		else if (arr[min] < n)
 //			left = min + 1;
 //		else
 //			break;
 //	}
 //	if (left <= right)
-//		printf("下标w为%d", min);
+//		printf("下标为%d", min);
 //	else
 //		printf("没有找到\n");
 //	return 0;
@@ -175,3 +176,246 @@
 //	}
 //	return 0;
 //}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int n = 100;
+//	for (int i = 1; i <= n; i++)
+//	{
+//		if (i % 3 == 0)
+//			printf("%d ", i);
+//	}
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b;
+//	int rem = 0;
+//	scanf("%d %d", &a, &b);
+//	while (b > 0)
+//	{
+//		rem = a % b;
+//		a = b;
+//		b = rem;
+//	}
+//	printf("%d", a);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	for (int i = 1000; i <= 2000; i++)
+//	{
+//		if ((i % 4 == 0 && i % 100 != 0) || i % 400 == 0)
+//			printf("%d ", i);
+//	}
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<math.h>
+//int main()
+//{
+//	for (int i = 100; i <= 200; i++)
+//	{
+//		int flag = 1;
+//		int tmp = (int)sqrt(i);
+//		for (int j = 2; j <= tmp; j++)
+//		{
+//			if (i%j == 0)
+//			{
+//				flag = 0;
+//				break;
+//			}
+//		}
+//		if (flag == 1)
+//			printf("%d ", i);
+//	}
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int sum = 0;
+//	for (int i = 0; i <= 100; i++)
+//	{
+//		int n = i;
+//		while (n)
+//		{
+//			if (n % 10 == 9)
+//				sum++;
+//			n /= 10;
+//		}
+//	}
+//	printf("0-100中有%d个9", sum);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	double sum = 0.0;
+//	double flag = 1.0;
+//	for (int i = 1; i <= 100; i++)
+//	{
+//		sum += 1 * flag / i;
+//		flag *= -1;
+//	}
+//	printf("%lf ", sum);
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	for (int i = 0; i < 10; i++)
+//		scanf("%d", &arr[i]);
+//	int max = arr[0];
+//	for (int i = 0; i < 10; i++)
+//	{
+//		if (arr[i] > max)
+//			max = arr[i];
+//	}
+//	printf("%d ", max);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	for (int i = 1; i <= 9; i++)
+//	{
+//		for (int j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%d\t", i, j, i*j);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[11], n;
+//	long long a, b, c;
+//	scanf("%d", &n);
+//	for (int i = 1; i <= n; i++)
+//	{
+//		scanf("%lld %lld %lld", &a, &b, &c);
+//		if (a + b > c)
+//			arr[i] = 1;
+//		else
+//			arr[i] = 0;
+//	}
+//	for (int i = 1; i <= n; i++)
+//	{
+//		if (arr[i])
+//			printf("Case #%d: true\n", i);
+//		else
+//			printf("Case #%d: false\n", i);
+//	}
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int n;
+//	scanf("%d", &n);
+//	while (n)
+//	{
+//		arr[n % 10]++;
+//		n /= 10;
+//	}
+//	for (int i = 0; i < 10; i++)
+//	{
+//		if (arr[i] != 0)
+//		{
+//			printf("%d:%d\n", i, arr[i]);
+//		}
+//	}
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//#include<string.h>
+//int main()
+//{
+//	char arr[1000];
+//	int sum[10] = { 0 };
+//	scanf("%s", arr);
+//	int size = strlen(arr);
+//	for (int i = 0; i < size; i++)
+//	{
+//		sum[arr[i] - '0']++;
+//	}
+//	for (int i = 0; i < 10; i++)
+//	{
+//		if (sum[i] != 0)
+//			printf("%d:%d\n", i, sum[i]);
+//
+//	}
+//	return 0;
+//}
+
+
+#include<stdio.h>
+int search(int* nums, int numsSize, int target)
+{
+	int left = 0, right = numsSize - 1;
+	int num = 0;
+	while (left <= right)
+	{
+		int min = (left + right) / 2;
+		if (nums[min] > target)
+			right = min - 1;
+		else if (nums[min] < target)
+			left = min + 1;
+		else
+		{
+			int flag = min - 1;
+			while (left <= flag)
+			{
+				if (nums[flag] == target)
+					num++;
+				else
+					break;
+				flag--;
+			}
+			flag = min + 1;
+			while (flag <= right)
+			{
+				if (nums[flag] == target)
+					num++;
+				else
+					break;
+				flag++;
+			}
+			num += 1;
+			return num;
+		}
+	}
+	return num;
+}
+int main()
+{
+	int arr[] = { 5,7,7,8,8,10 };
+	int target = 8;
+	int flag = search(arr, 6, target);
+	printf("%d", flag);
+	return 0;
+}
