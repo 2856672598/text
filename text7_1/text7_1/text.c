6037186d4c7dfc6449337aa9362999b4744eca71
2856672598 <2856672598@qq.com>
2021-07-02 17:23:07 +0800

    矩阵中的路径

--- text7_1/text7_1/text.c
+++ text7_1/text7_1/text.c
@@ -20,9 +20,9 @@ bool IsPath(char** board, int n, int m, char* word, int wordsz, int insert, Seat
 }
 bool Path(char** board, int n, int m, char* word, int wordsz, int insert, Seat pos)
 {
-	board[pos.row][pos.col] = '*';
 	if (board[pos.row][pos.col] == word[wordsz - 1] && insert == wordsz - 1)
 		return true;
+	board[pos.row][pos.col] = '*';
 	Seat next;
 	//上
 	next.row = pos.row - 1;
@@ -56,6 +56,8 @@ bool Path(char** board, int n, int m, char* word, int wordsz, int insert, Seat p
 		if (Path(board, n, m, word, wordsz, insert + 1, next))
 			return true;
 	}
+
+	board[pos.row][pos.col] = word[insert];
 	return false;
 }
 
@@ -63,25 +65,44 @@ bool exist(char** board, int boardSize, int* boardColSize, char* word)
 {
 	int row = boardSize;
 	int wordsz = strlen(word);
-	Seat pos = { 0,0 };
 	int insert = 0;
-	if (Path(board, row, *boardColSize, word, wordsz, insert, pos))
-		return true;
-	else
-		return false;
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < *boardColSize; j++)
+		{
+			Seat pos = { i,j };
+			if (board[i][j] == word[0])
+			{
+				if (Path(board, row, *boardColSize, word, wordsz, insert, pos))
+					return true;
+			}
+		}
+	}
+	return false;
 }
 int main()
 {
 	//char arr[3][4] = { {'A','B','C','E' }, {'S', 'F', 'C', 'S'},{'A', 'D', 'E', 'E'} };
-	char word[] = "AAA";
-	char**arr = malloc(sizeof(char*) * 3);
-	for (int i = 0; i < 3; i++)
+	char word[] = "ABCCED";
+	int row = 3, col = 4;
+	char**arr = malloc(sizeof(char*) * row);
+	for (int i = 0; i < row; i++)
 	{
-		arr[i] = malloc(sizeof(char) * 4);
-		scanf("%s", arr[i]);
+		//每行输入完后要回车
+		arr[i] = malloc(sizeof(char) * col);
+		for (int j = 0; j < col; j++)
+		{
+			scanf("%c", &arr[i][j]);
+		}
+		getchar();
 	}
-	int col = 4;
-	int flag = exist(arr, 3, &col, word);
+	int flag = exist(arr, row, &col, word);
 	printf("%d ", flag);
+	for (int i = 0; i < row; i++)
+	{
+		free(arr[i]);
+	}
+
+	free(arr);
 	return 0;
 }
