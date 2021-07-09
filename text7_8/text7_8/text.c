1de6da9d2fc19177e9ee56bab59bea2028149c44
2856672598 <2856672598@qq.com>
2021-07-09 21:20:29 +0800

    7/9练习

--- text7_8/text7_8/text.c
+++ text7_8/text7_8/text.c
@@ -234,83 +234,317 @@
 //	return 0;
 //}
 
-#define _CRT_SECURE_NO_WARNINGS
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+//void Sort(int* nums, int size)
+//{
+//	for (int i = 0; i < size - 1; i++)
+//	{
+//		int end = i, flag = nums[end + 1];
+//		while (end >= 0)
+//		{
+//			if (nums[end] < flag)
+//			{
+//				nums[end + 1] = nums[end];
+//				end--;
+//			}
+//			else
+//				break;
+//		}
+//		nums[end + 1] = flag;
+//	}
+//}
+//int Find(int* score, int left, int right, int target)
+//{
+//	int min = 0;
+//	while (left <= right)
+//	{
+//		min = left + ((right - left) >> 1);
+//		if (score[min] > target)
+//		{
+//			left = min + 1;
+//		}
+//		else if (score[min] < target)
+//			right = min - 1;
+//		else
+//			break;
+//	}
+//	return min;
+//}
+//char ** findRelativeRanks(int* score, int scoreSize, int* returnSize)
+//{
+//	int* arr = malloc(sizeof(int)*scoreSize);
+//	for (int i = 0; i < scoreSize; i++)
+//		arr[i] = score[i];
+//	char** ret = malloc(sizeof(char*) * scoreSize);
+//	Sort(score, scoreSize);
+//	for (int i = 0; i < scoreSize; i++)
+//	{
+//		int pos = Find(score, 0, scoreSize - 1, arr[i]);
+//		ret[i] = malloc(sizeof(char) * 15);
+//		switch (pos)
+//		{
+//		case 0:
+//			sprintf(ret[i], "Gold Medal");
+//			break;
+//		case 1:
+//			sprintf(ret[i], "Silver Medal");
+//			break;
+//		case 2:
+//			sprintf(ret[i], "Bronze Medal");
+//			break;
+//		default:
+//			sprintf(ret[i], "%d", pos + 1);
+//			break;
+//		}
+//	}
+//	free(arr);
+//	*returnSize = scoreSize;
+//	return ret;
+//}
+//int main()
+//{
+//	int nums[] = { 5,4,9,10,15,3,2,1 };
+//	int sz = sizeof(nums) / sizeof(nums[0]);
+//	int returnSize = 0;
+//	char** ret = findRelativeRanks(nums, sz, &returnSize);
+//	for (int i = 0; i < returnSize; i++)
+//	{
+//		printf("%s\n", ret[i]);
+//		free(ret[i]);
+//	}
+//	free(ret);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//void reverseString(char* s, int sSize)
+//{
+//	int left = 0, right = sSize - 1;
+//	while (left < right)
+//	{
+//		char tmp = s[left];
+//		s[left] = s[right];
+//		s[right] = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//int main()
+//{
+//	char arr[] = "helloword";
+//	int sz = strlen(arr);
+//	reverseString(arr, sz);
+//	printf("%s", arr);
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//void rever(char* s, int left, int right)
+//{
+//	while (left < right)
+//	{
+//		char tmp = s[left];
+//		s[left] = s[right];
+//		s[right] = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//char * reverseWords(char * s)
+//{
+//	int sz = strlen(s);
+//	int begin = 0, end = 0;
+//	while (end < sz)
+//	{
+//		if (s[end] != ' ')
+//			end++;
+//		else
+//		{
+//			rever(s, begin, end-1);
+//			end++;
+//			begin = end;
+//		}
+//	}
+//	rever(s, begin, end - 1);
+//	return s;
+//}
+//int main()
+//{
+//	char arr[] = "Let's take LeetCode contest";
+//	reverseWords(arr);
+//	printf("%s", arr);
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//void Sort(int* nums, int sz)
+//{
+//	for (int i = 0; i < sz - 1; i++)
+//	{
+//		int end = i, flag = nums[end + 1];
+//		while (end >= 0)
+//		{
+//			if (nums[end] > flag)
+//			{
+//				nums[end + 1] = nums[end];
+//				end--;
+//			}
+//			else
+//				break;
+//		}
+//		nums[end + 1] = flag;
+//	}
+//}
+//int maxProductDifference(int* nums, int numsSize)
+//{
+//	Sort(nums, numsSize);
+//	return nums[numsSize - 1] * nums[numsSize - 2] - nums[0] * nums[1];
+//}
+//int main()
+//{
+//	int nums[] = { 4,2,5,9,7,4,8 };
+//	int sz = sizeof(nums) / sizeof(nums[0]);
+//	int max = maxProductDifference(nums, sz);
+//	printf("%d", max);
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//void Sort(int* nums, int sz)
+//{
+//	for (int i = 0; i < sz - 1; i++)
+//	{
+//		int end = i, flag = nums[end + 1];
+//		while (end >= 0)
+//		{
+//			if (nums[end] > flag)
+//			{
+//				nums[end + 1] = nums[end];
+//				end--;
+//			}
+//			else
+//				break;
+//		}
+//		nums[end + 1] = flag;
+//	}
+//}
+//int* findErrorNums(int* nums, int numsSize, int* returnSize)
+//{
+//	*returnSize = 2;
+//	Sort(nums, numsSize);
+//	int* ret = malloc(sizeof(int) * 2);
+//	int prev = 0;
+//	int count = 0;
+//	for (int i = 0; i < numsSize; i++)
+//	{
+//		int cur = nums[i];
+//		if (cur == prev)
+//		{
+//			ret[0] = prev;
+//		}
+//		prev = cur;
+//		count += cur;
+//	}
+//	ret[1] = abs((1 + numsSize)*numsSize / 2 - count + ret[0]);
+//	return ret;
+//}
+//int main()
+//{
+//	int nums[] = { 1,2,2,4 };
+//	int returnSize = 0;
+//	int sz = sizeof(nums) / sizeof(nums[0]);
+//	int* ret = findErrorNums(nums, sz, &returnSize);
+//	for (int i = 0; i < returnSize; i++)
+//		printf("%d ", ret[i]);
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int mySqrt(int x)
+//{
+//	int left = 0, right = x;
+//	int ans = -1;
+//	while (left <= right)
+//	{
+//		long long min = left + (right - left) / 2;
+//		if (min*min > x)
+//			right = min - 1;
+//		else
+//		{
+//			//找最接近x的
+//			ans = min;
+//			left = min + 1;
+//		}
+//	}
+//	return ans;
+//}
+//int main()
+//{
+//	int x = 12;
+//	printf("%d", mySqrt(x));
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int arrangeCoins(int n)
+//{
+//	int i = 1, sum = 0;
+//	while (sum <= n - i)
+//	{
+//		sum += i;
+//		i++;
+//	}
+//	return i - 1;
+//}
+//int main()
+//{
+//	int n = 10;
+//	printf("%d ", arrangeCoins(n));
+//	return 0;
+//}
+
+//852. 山脉数组的峰顶索引
 #include<stdio.h>
-#include<stdlib.h>
-void Sort(int* nums, int size)
+//int peakIndexInMountainArray(int* arr, int arrSize)
+//{
+//	int maxi = 0;
+//	for (int i = 0; i < arrSize; i++)
+//	{
+//		if (arr[i] > arr[maxi])
+//			maxi = i;
+//	}
+//	return maxi;
+//}
+
+//二分
+int peakIndexInMountainArray(int* arr, int arrSize)
 {
-	for (int i = 0; i < size - 1; i++)
+	int left = 0, right = arrSize - 2, maxi;
+	while (left < right)
 	{
-		int end = i, flag = nums[end + 1];
-		while (end >= 0)
+		int min = left + (right - left) / 2;
+		if (arr[min] < arr[min + 1])
 		{
-			if (nums[end] < flag)
-			{
-				nums[end + 1] = nums[end];
-				end--;
-			}
-			else
-				break;
-		}
-		nums[end + 1] = flag;
-	}
-}
-int Find(int* score, int left, int right, int target)
-{
-	int min = 0;
-	while (left <= right)
-	{
-		min = left + ((right - left) >> 1);
-		if (score[min] > score[right])
-		{
-			right = min - 1;
-		}
-		else if (score[min] < score[left])
+			maxi = min;
 			left = min + 1;
-		else
-			break;
-	}
-	return min;
-}
-char ** findRelativeRanks(int* score, int scoreSize, int* returnSize)
-{
-	int* arr = malloc(sizeof(int)*scoreSize);
-	for (int i = 0; i < scoreSize; i++)
-		arr[i] = score[i];
-	char** ret = malloc(sizeof(char*) * scoreSize);
-	Sort(score, scoreSize);
-	for (int i = 0; i < scoreSize; i++)
-	{
-		int pos = Find(score, i, scoreSize - 1, arr[i]);
-		ret[i] = malloc(sizeof(char) * 15);
-		switch (pos)
-		{
-		case 0:
-			sprintf(ret[i], "Gold Medal");
-			break;
-		case 1:
-			sprintf(ret[i], "Silver Medal");
-			break;
-		case 2:
-			sprintf(ret[i], "Bronze Medal");
-			break;
-		default:
-			sprintf(ret[i], "%d", i + 1);
-			break;
 		}
+		else
+			right = min - 1;
 	}
-	free(arr);
-	*returnSize = scoreSize;
-	return ret;
+	return maxi + 1;
 }
 int main()
 {
-	int nums[] = { 5,4,3,2,1 };
-	int sz = sizeof(nums) / sizeof(nums[0]);
-	int returnSize = 0;
-	char** ret = findRelativeRanks(nums, sz, &returnSize);
-	for (int i = 0; i < returnSize; i++)
-	{
-		printf("%s\n", ret[i]);
-	}
+	int arr[] = { 24,69,100,99,79,78,67,36,26,19 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int maxi = peakIndexInMountainArray(arr, sz);
+	printf("%d", maxi);
+	return 0;
 }
