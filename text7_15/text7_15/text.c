9a5b988625afd81980697d4b571e6ed089267ff3
2856672598 <2856672598@qq.com>
2021-07-16 20:01:51 +0800

    7/16代码

--- text7_15/text7_15/text.c
+++ text7_15/text7_15/text.c
@@ -149,17 +149,153 @@
 //}
 
 
-#include<stdio.h>
-char * reverseVowels(char * s)
-{
-	int size = strlen(s);
-	int left = 0, right = size - 1;
-	while (left < right)
-	{
+////345. 反转字符串中的元音字母
+//#include<stdio.h>
+//#include<stdbool.h>
+//#include<string.h>
+//bool IsVowel(char ch)
+//{
+//	if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+//		ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+//		return true;
+//	else
+//		return false;
+//}
+//char * reverseVowels(char * s)
+//{
+//	int size = strlen(s);
+//	int left = 0, right = size - 1;
+//	while (left < right)
+//	{
+//		if (IsVowel(s[left]) && IsVowel(s[right]))
+//		{
+//			char tmp = s[left];
+//			s[left] = s[right];
+//			s[right] = tmp;
+//			left++;
+//			right--;
+//		}
+//
+//		if (!IsVowel(s[left]))
+//			left++;
+//		if (!IsVowel(s[right]))
+//			right--;
+//	}
+//	return s;
+//}
+//int main()
+//{
+//	char arr[] = "leetcode";
+//	reverseVowels(arr);
+//	printf("%s", arr);
+//	return 0;
+//}
 
+
+//#include<stdio.h>
+//#include<string.h>
+//int strStr(char * haystack, char * needle)
+//{
+//	if (strlen(haystack) == 0 || strlen(needle) == 0)
+//		return 0;
+//	int begin1 = 0, begin2 = 0;
+//	while (begin1 < (int)strlen(haystack))
+//	{
+//		begin2 = 0;
+//		if (haystack[begin1] == needle[begin2])
+//		{
+//			int cur = begin1;
+//			while (begin2 < (int)strlen(needle))
+//			{
+//				if (haystack[cur] == needle[begin2])
+//				{
+//					cur++;
+//					begin2++;
+//				}
+//				else
+//					break;
+//			}
+//			if (begin2 >= (int)strlen(needle))
+//				return begin1;
+//		}
+//		begin1++;
+//	}
+//	return -1;
+//}
+//int main()
+//{
+//	char arr1[] = "";
+//	char arr2[] = "a";
+//	int res = strStr(arr1, arr2);
+//	printf("%d", res);
+//	return 0;
+//}
+
+
+//1346. 检查整数及其两倍数是否存在
+#include<stdio.h>
+#include<stdbool.h>
+//bool checkIfExist(int* arr, int arrSize)
+//{
+//	for (int i = 0; i < arrSize; i++)
+//	{
+//		for (int j = 0; j < arrSize; j++)
+//		{
+//			if (i != j && arr[i] * 2 == arr[j])
+//				return true;
+//		}
+//	}
+//	return false;
+//}
+
+//1346. 检查整数及其两倍数是否存在
+void Sort(int* arr, int size)
+{
+	for (int i = 0; i < size - 1; i++)
+	{
+		int end = i, flag = arr[end + 1];
+		while (end >= 0)
+		{
+			if (arr[end] > flag)
+			{
+				arr[end + 1] = arr[end];
+				end--;
+			}
+			else
+				break;
+		}
+		arr[end + 1] = flag;
 	}
+}
+int Find(int* arr, int size, int target)
+{
+	int left = 0, right = size - 1;
+	while (left <= right)
+	{
+		int mid = left + (right - left) / 2;
+		if (arr[mid] > target)
+			right = mid - 1;
+		else if (arr[mid] < target)
+			left = mid + 1;
+		else
+			return mid;
+	}
+	return -1;
+}
+bool checkIfExist(int* arr, int arrSize)
+{
+	Sort(arr, arrSize);
+	for (int i = 0; i < arrSize; i++)
+	{
+		int pos = Find(arr, arrSize, arr[i] * 2);
+		if (pos != -1 && pos != i)
+			return true;
+	}
+	return false;
 }
 int main()
 {
+	int arr[] = { -2,0,10,-19,4,6,-8 };
+	checkIfExist(arr, 7);
 	return 0;
 }
