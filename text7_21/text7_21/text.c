6417cdffe6a9f235a53a5a22a460f8aa20b62e44
2856672598 <2856672598@qq.com>
2021-07-23 20:54:36 +0800

    7/23练习

--- text7_21/text7_21/text.c
+++ text7_21/text7_21/text.c
@@ -345,39 +345,348 @@
 //	return false;
 //}
 
-
-#include<stdio.h>
-#include<stdbool.h>
+////81. 搜索旋转排序数组 II
+//#include<stdio.h>
+//#include<stdbool.h>
+////bool search(int* nums, int numsSize, int target)
+////{
+////	for (int i = 0; i < numsSize; i++)
+////		if (nums[i] == target)
+////			return true;
+////	return false;
+////}
+//
 //bool search(int* nums, int numsSize, int target)
 //{
-//	for (int i = 0; i < numsSize; i++)
-//		if (nums[i] == target)
+//	int left = 0, right = numsSize - 1;
+//	while (left <= right)
+//	{
+//		int mid = left + (right - left) / 2;
+//		if (nums[mid] == target)
 //			return true;
+//		if (nums[mid] == nums[left] && nums[mid] == nums[right])
+//		{
+//			left++;
+//			right--;
+//		}
+//		else if (nums[mid] >= nums[left])
+//		{
+//			if (nums[left] <= target && nums[mid] > target)
+//				right = mid - 1;
+//			else
+//				left = mid + 1;
+//		}
+//		else
+//		{
+//			if (nums[mid] < target&&target <= nums[right])
+//				left = mid + 1;
+//			else
+//				right = mid - 1;
+//		}
+//	}
 //	return false;
 //}
+//int main()
+//{
+//	int nums[] = { 3,1 };
+//	int target = 1;
+//	int size = sizeof(nums) / sizeof(nums[0]);
+//	printf("%d", search(nums, size, target));
+//	return 0;
+//}
 
-bool search(int* nums, int numsSize, int target)
+//#include<stdio.h>
+//int findMin(int* nums, int numsSize)
+//{
+//	int min = nums[0];
+//	for (int i = 0; i < numsSize; i++)
+//	{
+//		if (nums[i] < min)
+//			min = nums[i];
+//	}
+//	return min;
+//}
+//int main()
+//{
+//	int nums[] = { 3,4,5,1,2 };
+//	int size = sizeof(nums) / sizeof(nums[0]);
+//	printf("%d", findMin(nums, size));
+//	return 0;
+//}
+
+//#include<stdio.h>
+//void Prin(unsigned int nums)
+//{
+//	if (nums > 9)
+//		Prin(nums / 10);
+//	printf("%d", nums % 10);
+//}
+//int main()
+//{
+//	unsigned int nums = 0;
+//	scanf("%u", &nums);
+//	Prin(nums);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int my_Strlen(char* nums)
+//{
+//	if (*nums != '\0')
+//		return my_Strlen(nums + 1) + 1;
+//	return  0;
+//}
+//int main()
+//{
+//	char nums[] = "hello";
+//	printf("%d", my_Strlen(nums));
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int findMin(int* nums, int numsSize)
+//{
+//	int left = 0, right = numsSize - 1, mid = 0;
+//	while (left < right)
+//	{
+//		mid = left + (right - left) / 2;
+//		//说明最小值在left-mid间
+//		if (nums[mid] < nums[right])
+//			right = mid;
+//		else//最小值在mid-right之间
+//			left = mid + 1;
+//	}
+//	return nums[left];
+//}
+//int main()
+//{
+//	int nums[] = { 3,4,5,1,2 };
+//	int size = sizeof(nums) / sizeof(nums[0]);
+//	printf("%d", findMin(nums, size));
+//	return 0;
+//}
+
+
+////162. 寻找峰值
+//#include<stdio.h>
+//int findPeakElement(int* nums, int numsSize)
+//{
+//	int curi = 0, previ = 0, nexti = 0;
+//	if (numsSize <= 1)
+//		return 0;
+//	if (numsSize == 2)
+//		return nums[0] < nums[1] ? nums[1] : nums[0];
+//	if (nums[0] > nums[1])
+//		return nums[0];
+//	if (nums[numsSize - 1] > nums[numsSize - 2])
+//		return nums[numsSize - 1];
+//
+//	for (int i = 1; i < numsSize - 1; i++)
+//	{
+//		curi = i;
+//		nexti = curi + 1;
+//		if (nums[curi] > nums[previ] && nums[curi] > nums[nexti])
+//			break;
+//		previ = curi;
+//	}
+//	return curi;
+//}
+//int main()
+//{
+//	int nums[] = { 1,6,6,4,5 };
+//	int size = sizeof(nums) / sizeof(nums[0]);
+//	printf("%d", findPeakElement(nums, size));
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//void  Print(int n)
+//{
+//	if (n > 9)
+//		Print(n / 10);
+//	printf("%d ", n % 10);
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	Print(n);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int Fac(int n)
+//{
+//	int sum = 1;
+//	while (n)
+//	{
+//		sum *= n;
+//		n--;
+//	}
+//	return sum;
+//}
+//int Fac1(int n)
+//{
+//	if (n > 1)
+//		return n * Fac1(n - 1);
+//	else
+//		return 1;
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	printf("%d", Fac1(n));
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int My_Strlen(char* nums)
+//{
+//	int count = 0;
+//	while (*nums != '\0')
+//	{
+//		count++;
+//		nums++;
+//	}
+//	return count;
+//}
+//int My_Strlen1(char* nums)
+//{
+//	if (*nums != '\0')
+//		return 1 + My_Strlen1(nums + 1);
+//	else
+//		return 0;
+//}
+//int main()
+//{
+//	char nums[] = "hello";
+//	int len = My_Strlen1(nums);
+//	printf("%d", len);
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int DigitSum(n)
+//{
+//	int sum = 0;
+//	if (n > 9)
+//		sum = DigitSum(n / 10);
+//	return sum + n % 10;
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	printf("%d", DigitSum(n));
+//	return 0;
+//}
+
+//#include<stdio.h>
+//double My_Pow(double n, double k)
+//{
+//	if (k)
+//		return n * My_Pow(n, k - 1);
+//	else
+//		return 1;
+//}
+//int main()
+//{
+//	double n, k;
+//	scanf("%lf %lf", &n, &k);
+//	printf("%lf", My_Pow(n, k));
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int Fib(int n)
+//{
+//	if (n <= 2)
+//		return 1;
+//	else
+//		return Fib(n - 1) + Fib(n - 2);
+//}
+//int Fib1(int n)
+//{
+//	int a = 1, b = 1, c = 1;
+//	while (n > 2)
+//	{
+//		c = a + b;
+//		b = a;
+//		a = c;
+//		n--;
+//	}
+//	return c;
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	printf("%d", Fib1(n));
+//	return 0;
+//}
+
+//209. 长度最小的子数组
+#include<stdio.h>
+#include<limits.h>
+//int minSubArrayLen(int target, int* nums, int numsSize)
+//{
+//	int count = INT_MAX;
+//	for (int i = 0; i < numsSize; i++)
+//	{
+//		int sum = nums[i];
+//		int j = i;
+//		if (nums[i] < target)
+//		{
+//			for (j = i + 1; j < numsSize && sum < target; j++)
+//			{
+//				sum += nums[j];
+//				if (sum >= target)
+//					break;
+//			}
+//		}
+//		if (j < numsSize && j - i + 1 < count)
+//			count = j - i + 1;
+//	}
+//	if (count == INT_MAX)
+//		count = 0;
+//	return count;
+//}
+
+//滑动窗口
+int minSubArrayLen(int target, int* nums, int numsSize)
 {
-	int left = 0, right = numsSize - 1;
-	while (left <= right)
+	if (numsSize == 0)
+		return 0;
+	int number = INT_MAX;
+	int begin = 0, end = 0;
+	int sum = 0;
+	while (end <= numsSize - 1)
 	{
-		int mid = left + (right - left) / 2;
-		if (nums[mid] == target)
-			return true;
-		if (nums[mid] > nums[0])
+		sum += nums[end];
+		while (sum >= target)
 		{
-			if (nums[mid] < target)
-				left = mid + 1;
-			else
-				right = mid - 1;
+			if (end - begin + 1 < number)
+				number = end - begin + 1;
+			sum -= nums[begin];
+			begin++;
 		}
-		else
-		{
-			if (nums[mid] < target)
-				left = mid + 1;
-			else
-				right = mid - 1;
-		}
+		end++;
 	}
-	return false;
+	if (number == INT_MAX)
+		number = 0;
+	return number;
+}
+
+int main()
+{
+	int nums[] = { 5,1,3,5,10,7,4,9,2,8 };
+	int target = 15;
+	int size = sizeof(nums) / sizeof(nums[0]);
+	printf("%d", minSubArrayLen(target, nums, size));
+	return 0;
 }
