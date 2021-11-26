06a6162fd1b9bd3b5a092d621d145758b51f8b9c
2856672598 <2856672598@qq.com>
2021-11-26 23:30:40 +0800

    练习

--- text_11_23/text_11_23/text.cpp
+++ text_11_23/text_11_23/text.cpp
@@ -239,32 +239,208 @@
 //}
 
 
+//#include<iostream>
+//#include<vector>
+//#include<algorithm>
+//using namespace std;
+//class Solution
+//{
+//public:
+//	int maxDistance(vector<int>& colors)
+//	{
+//		int n = colors.size();
+//		int ret = 0;
+//		for (int i = 0; i < n; i++)
+//		{
+//			for (int j = i + 1; j < n; j++)
+//			{
+//				if (colors[i] != colors[j])
+//					ret = max(ret, j - i);
+//			}
+//		}
+//		return ret;
+//	}
+//};
+//
+//int main()
+//{
+//	vector<int>colors{ 1,1,1,6,1,1,1 };
+//	cout << Solution().maxDistance(colors) << endl;
+//	return 0;
+//}
+
+
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//
+//void Add(vector<int>& a1, vector<int>& a2, vector<int>& a1result)
+//{
+//	int sum = 0;
+//	for (int i = 0; i < (int)a2.size(); i++)
+//	{
+//		sum = a2[i] + a1[i];
+//		a1result.push_back(sum);
+//	}
+//}
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	vector<int>a1, a2;
+//	vector<int>a1result;
+//	a1.resize(n);
+//	a2.resize(n);
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> a1[i];
+//	}
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> a2[i];
+//	}
+//	Add(a1, a2, a1result);
+//	for (int i = 0; i < n; i++)
+//		cout << a1result[i] << " ";
+//	return 0;
+//}
+
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//
+//void compar(vector<int>& nums, vector<int>flag)
+//{
+//	for (int i = flag[2] - 1; i <= flag[3] - 1; i++)
+//	{
+//		int count = 0;
+//		for (int j = flag[0] - 1; j <= flag[1] - 1; j++)
+//		{
+//			if (nums[i] >= nums[j])
+//				count++;
+//		} 
+//		cout << count << " ";
+//	}
+//	cout << endl;
+//}
+//
+//int main()
+//{
+//	int n, m;
+//	cin >> n >> m;
+//	vector<int> nums;
+//	vector<vector<int> >flag;
+//	nums.resize(n);
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> nums[i];
+//	}
+//
+//	for (int i = 0; i < m; i++)
+//	{
+//		vector<int>tmp;
+//		tmp.resize(4);
+//		for (int j = 0; j < 4; j++)
+//		{
+//			cin >> tmp[j];
+//		}
+//		flag.push_back(tmp);
+//	}
+//	for (int i = 0; i < m; i++)
+//	{
+//		compar(nums, flag[i]);
+//	}
+//	return 0;
+//}
+
+
+//#include <iostream>
+//using namespace std;
+//// 比较两个字符串是否相等，如果相等，返回0，如果不等，返回一个非零的值。
+//int strcmp(char *a, char *b)
+//{
+//	if (strlen(a) != strlen(b))
+//		return 1;
+//	while (*a == *b&&*a != '\0')
+//	{
+//		a++;
+//		b++;
+//	}
+//	if (*a == *b&&*a == '\0')
+//		return 0;
+//	return 1;
+//}
+//char a[10010], b[10010];
+//
+//int main()
+//{
+//	cin >> a >> b;
+//	if (strcmp(a, b) == 0)
+//		cout << "yes" << endl;
+//	else
+//		cout << "no" << endl;
+//	return 0;
+//}
+
+////217. 存在重复元素
+//#include<iostream>
+//#include<vector>
+//#include<unordered_set>
+//using namespace std;
+//
+//class Solution
+//{
+//public:
+//	bool containsDuplicate(vector<int>& nums)
+//	{
+//		unordered_set<int> flag;
+//		for (auto e : nums)
+//		{
+//			if (flag.find(e) != flag.end())
+//				return true;
+//			flag.insert(e);
+//		}
+//		return false;
+//	}
+//};
+//int main()
+//{
+//	vector<int>nums = { 1,1,1,3,3,4,3,2,4,2 };
+//	cout << Solution().containsDuplicate(nums) << endl;
+//	return 0;
+//}
+
+
 #include<iostream>
 #include<vector>
-#include<algorithm>
+#include<unordered_set>
 using namespace std;
+
 class Solution
 {
 public:
-	int maxDistance(vector<int>& colors)
+	bool containsNearbyDuplicate(vector<int>& nums, int k)
 	{
-		int n = colors.size();
-		int ret = 0;
-		for (int i = 0; i < n; i++)
+		unordered_set<int> s;
+		for (int i = 0; i < (int)nums.size(); i++)
 		{
-			for (int j = i + 1; j < n; j++)
+			if (s.find(nums[i]) != s.end())
+				return true;
+			s.insert(nums[i]);
+			if ((int)s.size() > k)
 			{
-				if (colors[i] != colors[j])
-					ret = max(ret, j - i);
+				//删除最早的一个
+				s.erase(nums[i - k]);
 			}
 		}
-		return ret;
+		return false;
 	}
 };
-
 int main()
 {
-	vector<int>colors{ 1,1,1,6,1,1,1 };
-	cout << Solution().maxDistance(colors) << endl;
+	vector<int>nums = { 1,0,1,1 };
+	int k = 1;
+	cout << Solution().containsNearbyDuplicate(nums, k) << endl;
 	return 0;
 }
