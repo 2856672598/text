d82e7a92f6896c6faeaf432795c6f750b39ede46
2856672598 <2856672598@qq.com>
2021-12-07 20:17:14 +0800

    练习

--- text_12_4/text_12_4/text1.cpp
+++ text_12_4/text_12_4/text1.cpp
@@ -87,46 +87,117 @@
 //	return 0;
 //}
 
-#define _CRT_SECURE_NO_WARNINGS
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<iostream>
+//#include<algorithm>
+//using namespace std;
+//
+//class String
+//{
+//public:
+//	String(const char* str = " ")
+//	{
+//		_str = new char[strlen(str) + 1];
+//		strcpy(_str, str);
+//	}
+//
+//	String(const String& s)
+//	{
+//		_str = new char[strlen(s._str) + 1];
+//		strcpy(_str, s._str);
+//		cout << "Éî¿½±´" << endl;
+//	}
+//	
+//	String(const String&& s)
+//	{
+//		_str = nullptr;
+//		cout << "Ç³¿½±´" << endl;
+//	}
+//
+//private:
+//	char* _str;
+//};
+//String f(const char* str)
+//{
+//	String tmp(str);
+//	return tmp;
+//}
+//int main()
+//{
+//	String s("hello word");
+//	String a(s);
+//	String b(f("ÁÙÊ±"));
+//
+//	return 0;
+//}
+
+
+//#include<iostream>
+//#include<vector>
+//#include<unordered_set>
+//#include<algorithm>
+//using namespace std;
+//
+//class Solution
+//{
+//public:
+//	int longestConsecutive(vector<int>& nums)
+//	{
+//		unordered_set<int>flag;
+//		for (auto e : nums)
+//		{
+//			flag.insert(e);
+//		}
+//		int maxleng = 0;
+//		int next = 0;
+//		int continu = 0;
+//		for (auto e : flag)
+//		{
+//			next = e + 1;
+//			continu = 1;
+//			while (flag.count(next))
+//			{
+//				next += 1;
+//				continu++;
+//			}
+//
+//			maxleng = max(maxleng, continu);
+//		}
+//		return maxleng;
+//	}
+//};
+//int main()
+//{
+//	vector<int >nums{ 100,4,200,1,3,2 };
+//	cout << Solution().longestConsecutive(nums) << endl;
+//	return 0;
+//}
+
+
 #include<iostream>
+#include<vector>
 #include<algorithm>
 using namespace std;
 
-class String
+class Solution
 {
 public:
-	String(const char* str = " ")
+	int kthSmallest(vector<vector<int>>& matrix, int k)
 	{
-		_str = new char[strlen(str) + 1];
-		strcpy(_str, str);
+		vector<int>flag;
+		for (int i = 0; i < matrix.size(); i++)
+		{
+			for (int j = 0; j < matrix[0].size(); j++)
+			{
+				flag.push_back(matrix[i][j]);
+			}
+		}
+		sort(flag.begin(), flag.end());
+		return flag[k - 1];
 	}
-
-	String(const String& s)
-	{
-		_str = new char[strlen(s._str) + 1];
-		strcpy(_str, s._str);
-		cout << "Éî¿½±´" << endl;
-	}
-	
-	String(const String&& s)
-	{
-		_str = nullptr;
-		cout << "Ç³¿½±´" << endl;
-	}
-
-private:
-	char* _str;
 };
-String f(const char* str)
-{
-	String tmp(str);
-	return tmp;
-}
+
 int main()
 {
-	String s("hello word");
-	String a(s);
-	String b(f("ÁÙÊ±"));
-
 	return 0;
 }
